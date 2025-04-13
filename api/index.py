a5204c95cca19ee91332a43ab2c467c2ba17e270
YogyashriPatil <yogyashripatil20@gmail.com>
2025-04-13 17:51:08 +0530

    Project completed

--- api/index.py
+++
@@ -1,5 +0,0 @@
-# api/index.py
-from chatbot_project.asgi import application  # change to your Django project's name
-
-async def handler(scope, receive, send):
-    await application(scope, receive, send)

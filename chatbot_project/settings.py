0ae3b2d353384cd72365c3486884d3ffe8f21a5f
YogyashriPatil <yogyashripatil20@gmail.com>
2025-04-13 18:03:13 +0530

    project completed

--- chatbot_project/settings.py
+++ chatbot_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-yyffab6=*(z4tk456&t(b0g8=+((9qeaono1cm5bjd0h**lrz_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['persona-ai-hitesh-piyush.onrender.com']
 
 
 # Application definition
